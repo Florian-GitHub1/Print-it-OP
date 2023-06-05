cda31181f8a2171ef556c0e0232e689983df50e6
Florian D <floriandouche@gmail.com>
2023-06-05 18:13:23 +0200

    Premier commit :     - Ajout des flèches en statique.     - Queryselector sur arrow_left / arrow_right / banner-img.     - Ajout des dots suivant la longeur du tableau.

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,36 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+let arrow_left = document.querySelector('.arrow_left');
+let arrow_right = document.querySelector('.arrow_right');
+let bannerImg = document.querySelector('.banner-img');
+
+arrow_left.addEventListener('click', () => {
+	console.log('click gauche')
+});
+
+arrow_right.addEventListener('click', () => {
+	console.log('click droit')
+});
+
+bannerImg.addEventListener('click', () => {
+	console.log('img')
+});
+
+
+
+function genererDots(slides) {
+	for (let i = 0; i < slides.length; i++) {
+
+		 const dotsElement = document.createElement("div");
+		 dotsElement.classList.add('dot');
+
+		 document.querySelector(".dots").appendChild(dotsElement);
+	}
+
+  }
+
+  genererDots(slides);
